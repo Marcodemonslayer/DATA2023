f6f22908130e070c27c749313094fbceec5ebf01
Marcodemonslayer <110564730+Marcodemonslayer@users.noreply.github.com>
2023-07-26 14:27:09 -0600

    Archive of the first class

---
+++ Repaso R/Archivo 26 de julio del 2023.R
@@ -0,0 +1,9 @@
+# Librerias que se pueden utilizar
+
+install.packages("lubridate")
+install.packages("stringr")
+installed.packages("readr")
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("RMySQL")
+install.packages("openxlsx")
